c07d6e611fd7a6946c65f42d476e3eff2d6a491d
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-03 00:38:12 +0530

    modi and kcr

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "modi:: hi kcr how are you?"
+echo "kcr:: hello modi, iam fine, how are you?"
+echo "modi:: iam fine too, what are you doing?"
+echo "kcr:: i am learning shell scripting, what about you?"
+echo "modi:: i am also learning shell scripting, it is very interesting."
