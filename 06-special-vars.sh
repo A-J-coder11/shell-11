9737f87e300fc0fda8582dc6c32259bfed1b7d90
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-03 13:58:44 +0530

    special-variables

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -8,3 +8,9 @@ echo "current user: $USER"
 echo "current shell: $SHELL"
 echo "current process ID: $$"
 sleep 10 &
+PID of th last running process: $!
+echo "Exit status of the last command: $?"
+echo "Home directory: $HOME"
+echo "Terminal device: $-"
+echo "Shell options: $-"
+echo "Current date and time: $(date)"
