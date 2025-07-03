a2f77e25098389009a8886cbf10f251b773189b2
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-03 13:51:07 +0530

    special-variables

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to the script are :: $@"
 echo "Number of Variables: $#"
+echo "Script Name: $0"
+echo "current directory: $PWD"
+echo "current user: $USER"
+echo "current shell: $SHELL"
+echo "current process ID: $$"
+sleep 10 &
