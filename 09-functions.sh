0b86c4d04dc625d2844c769bd459a9995c387def
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-05 22:53:07 +0530

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+# Root user check
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
   echo "You must be root to run this script."
@@ -9,6 +8,7 @@ else
   echo "You are root, proceeding with installation."
 fi
 
+# validate is a function and takes input as exit status and tries to install 
 VALIDATE(){
   if [ $1 -eq 0 ]; then
     echo "$2 installed successfully."
@@ -19,38 +19,28 @@ VALIDATE(){
 }
 
 dnf list installed mysql
-
 if [ $? -eq 0 ]; then
   echo "mysql is not installed going to inbstall it."
   dnf install mysql -y
-
-# Check if MySQL installation was successful
   VALIDATE $? "MySQL"
 else
-  echo "mysql is already installed."
-  
+  echo "mysql is already installed."  
 fi
 
 dnf list installed python3
 if [ $? -eq 0 ]; then
   echo "python3 is not installed going to install it."
   dnf install python3 -y
-
-# Check if Python3 installation was successful
   VALIDATE $? "Python3"
 else
   echo "python3 is already installed."
-
 fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]; then
   echo "nginx is not installed going to install it."
   dnf install nginx -y
-
-# Check if Nginx installation was successful
   VALIDATE $? "Nginx"
 else
   echo "nginx is already installed."
-
 fi
