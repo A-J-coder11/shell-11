f6ab817a14cdc05edcd38f57ec7fdb5dc7258a60
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-05 23:43:44 +0530

    functions

--- 11-colors.sh
+++ 11-colors.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 # Root user check
 USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+
 if [ $USERID -ne 0 ]; then
-  echo  -e "\e[32m You must be root to run this script.\e[0m"
+  echo  -e "$R You must be root to run this script$N"
   exit 1
 else
-  echo "You are root, proceeding with installation."
+  echo -e "$G You are root, proceeding with installation.$N"
 fi
 
 # validate is a function and takes input as exit status and tries to install 
 VALIDATE(){
   if [ $1 -eq 0 ]; then
-    echo "$2 installed successfully."
+    echo -e "$G $2 installed successfully.$N"
   else
-    echo "Failed to install $2."
+    echo -e "$R Failed to install $2.$N"
     exit 1
   fi
 }
 
 dnf list installed mysql
 if [ $? -eq 0 ]; then
-  echo "mysql is not installed going to inbstall it."
+  echo -e "$Y mysql is not installed going to install it.$N"
   dnf install mysql -y
   VALIDATE $? "MySQL"
 else
-  echo "mysql is already installed."  
+  echo -e "$G mysql is already installed.$N"
 fi
 
 dnf list installed python3
 if [ $? -eq 0 ]; then
-  echo "python3 is not installed going to install it."
+  echo -e "$Y python3 is not installed going to install it.$N"
   dnf install python3 -y
   VALIDATE $? "Python3"
 else
-  echo "python3 is already installed."
+  echo -e "$G python3 is already installed.$N"
 fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]; then
-  echo "nginx is not installed going to install it."
+  echo -e "$Y nginx is not installed going to install it.$N"
   dnf install nginx -y
   VALIDATE $? "Nginx"
 else
-  echo "nginx is already installed."
+  echo -e "$G nginx is already installed.$N"
 fi
