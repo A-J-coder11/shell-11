6ea292e052000815d1fc1399a9c0ba0ac78e4449
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-05 23:25:11 +0530

    functions

---
+++ 11-colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Root user check
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+  echo  -e "\e[32m You must be root to run this script.\e[0m"
+  exit 1
+else
+  echo "You are root, proceeding with installation."
+fi
+
+# validate is a function and takes input as exit status and tries to install 
+VALIDATE(){
+  if [ $1 -eq 0 ]; then
+    echo "$2 installed successfully."
+  else
+    echo "Failed to install $2."
+    exit 1
+  fi
+}
+
+dnf list installed mysql
+if [ $? -eq 0 ]; then
+  echo "mysql is not installed going to inbstall it."
+  dnf install mysql -y
+  VALIDATE $? "MySQL"
+else
+  echo "mysql is already installed."  
+fi
+
+dnf list installed python3
+if [ $? -eq 0 ]; then
+  echo "python3 is not installed going to install it."
+  dnf install python3 -y
+  VALIDATE $? "Python3"
+else
+  echo "python3 is already installed."
+fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+  echo "nginx is not installed going to install it."
+  dnf install nginx -y
+  VALIDATE $? "Nginx"
+else
+  echo "nginx is already installed."
+fi
