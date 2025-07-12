44a7aa98267317c78aaee3d005fd04845b3cc4e9
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-12 21:52:50 +0530

    loops

---
+++ 14-loops.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Root user check
+USERID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+LOGS_FOLDER="/var/log/shell22-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("$package" "python" "nginx" "httpd")
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+  echo  -e "$R You must be root to run this script$N" | tee -a $LOG_FILE
+  exit 1
+else
+  echo -e "$G You are root, proceeding with installation." | tee -a $LOG_FILE
+fi
+
+# validate is a function and takes input as exit status and tries to install 
+VALIDATE(){
+  if [ $1 -eq 0 ]; then
+    echo -e "$G $2 installed successfully.$N" | tee -a $LOG_FILE
+  else
+    echo -e "$R Failed to install $2.$N" | tee -a $LOG_FILE
+    exit 1
+  fi
+}
+
+for Package in ${PACKAGES[@]}
+do
+    dnf list installed $Package &>>$LOG_FILE
+    if [ $? -eq 0 ]; then
+      echo -e "$Y $package is not installed going to install it.$N" | tee -a $LOG_FILE
+      dnf install $package -y &>>$LOG_FILE
+      VALIDATE $? "$package" 
+    else
+      echo -e "$G $package is already installed.$N" | tee -a $LOG_FILE
+    fi
+done
+
+
