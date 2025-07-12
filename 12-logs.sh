e144055e8e132c4228e9bc48763c3e6863659d91
A-J-coder11 <anand.coder.j@gmail.com>
2025-07-12 13:05:32 +0530

    logs

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,11 +5,12 @@ R='\e[31m'
 G='\e[32m'
 Y='\e[33m'
 N='\e[0m'
-LOGS_FOLDER="/Var/log/shell22-logs"
+LOGS_FOLDER="/var/log/shell22-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
+
 echo "Script started executing at: $(date)" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]; then
